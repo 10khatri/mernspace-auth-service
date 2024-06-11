5603676af6f7b01fce2d730c0e90fafbe5356b4e
10khatri <f1footballfreestylers@gmail.com>
2024-06-11 17:37:37 +0530

    husky test 4.0

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
 function login(username: string): string {
     const user = {
         username: "abhijit",
-    };const name = user["usernam"];
+    };
+    const name = user.username;
     return username + name;
 }
 login("admin");
