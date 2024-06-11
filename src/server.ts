58ce6630f54004480ff931b3adb820a1936f38fe
10khatri <f1footballfreestylers@gmail.com>
2024-06-11 18:16:34 +0530

    add config setup

--- src/server.ts
+++
@@ -1,8 +0,0 @@
-function login(username: string): string {
-    const user = {
-        username: "abhijit",
-    };
-    const name = user.username;
-    return username + name;
-}
-login("admin");
