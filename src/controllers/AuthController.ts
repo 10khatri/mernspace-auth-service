a23d35ff629557cecedb3414e2b06ecd1e2cdf43
10khatri <f1footballfreestylers@gmail.com>
2024-06-15 14:49:16 +0530

    add test for persist user

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,27 +1,19 @@
-import { Request, Response } from "express";
-import { AppDataSource } from "../config/data-source";
-import { User } from "../entity/User";
+import { Response } from "express";
 
-interface UserData {
-    firstName: string;
-    lastName: string;
-    email: string;
-    password: string;
-}
-interface RegisterUserRequest extends Request {
-    body: UserData;
-}
+import { RegisterUserRequest } from "../types";
+import { UserService } from "../services/UserService";
 
 export class AuthController {
+    userService: UserService;
+    constructor(userService: UserService) {
+        this.userService = userService;
+    }
+
     async register(req: RegisterUserRequest, res: Response) {
         const { firstName, lastName, email, password } = req.body;
-        const userRepository = AppDataSource.getRepository(User);
-        await userRepository.save({
-            firstName,
-            lastName,
-            email,
-            password,
-        });
+
+        await this.userService.create({ firstName, lastName, email, password });
+
         res.status(201).json();
     }
 }
