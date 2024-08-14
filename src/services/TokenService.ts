0d22840daa1139e09124541c82081246e9649f49
10khatri <f1footballfreestylers@gmail.com>
2024-08-14 13:44:02 +0530

    fix refresh token

--- src/services/TokenService.ts
+++ src/services/TokenService.ts
@@ -1,3 +1,5 @@
+import fs from "node:fs";
+import path from "node:path";
 import { JwtPayload, sign } from "jsonwebtoken";
 import createHttpError from "http-errors";
 import { Config } from "../config";
@@ -8,14 +10,12 @@ import { Repository } from "typeorm";
 export class TokenService {
     constructor(private refreshTokenRepository: Repository<RefreshToken>) {}
     generateAccessToken(payload: JwtPayload) {
-        let privateKey: string;
-        if (!Config.PRIVATE_KEY) {
-            const error = createHttpError(500, "Secret key is not set");
-            throw error;
-        }
+        let privateKey: Buffer;
 
         try {
-            privateKey = Config.PRIVATE_KEY;
+            privateKey = fs.readFileSync(
+                path.join(__dirname, "../../certs/private.pem"),
+            );
         } catch (err) {
             const error = createHttpError(
                 500,
