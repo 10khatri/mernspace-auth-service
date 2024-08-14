77ac05465a89e4c535a24b54978b3939cd2e7bc2
10khatri <f1footballfreestylers@gmail.com>
2024-08-14 12:26:11 +0530

    add cors

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,16 @@ import express, { NextFunction, Request, Response } from "express";
 import logger from "./config/logger";
 import cookieParser from "cookie-parser";
 import { HttpError } from "http-errors";
+import cors from "cors";
 import authRouter from "./routes/auth";
 const app = express();
+
+app.use(
+    cors({
+        origin: ["http://localhost:5173"],
+        credentials: true,
+    }),
+);
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.static("public"));
